463b0251487ba292a5f646f9d0fc133b161d7f4d
yangy <yangy@DESKTOP-BC9KLPC>
2018-04-27 11:03:58 +0800

    small change.

--- src/main/java/randoop/generation/date/tensorflow/QLearning.java
+++ src/main/java/randoop/generation/date/tensorflow/QLearning.java
@@ -1,19 +1,35 @@
 package randoop.generation.date.tensorflow;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.tensorflow.Graph;
 import org.tensorflow.Session;
-import org.tensorflow.Tensor;
-import org.tensorflow.TensorFlow;
+
+import com.google.common.io.ByteStreams;
 
 public class QLearning {
 	
+	Graph graph = new Graph();
+	Session session = new Session(graph);
+
 	public QLearning() {
-	}
-	
-	public void QLearn() {
-		try (Graph g = new Graph()) {
-			
+		// import model
+		try {
+			graph.importGraphDef(LoadGraphDef());
+		} catch (IllegalArgumentException | IOException e) {
+			e.printStackTrace();
 		}
 	}
-	
+
+	private byte[] LoadGraphDef() throws IOException {
+		try (InputStream is = QLearning.class.getClassLoader().getResourceAsStream("refined_deep_q.pb")) {
+			return ByteStreams.toByteArray(is);
+		}
+	}
+
+	public void QLearn() {
+		
+	}
+
 }
