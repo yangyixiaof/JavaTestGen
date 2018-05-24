eb171b38e52b9834b1f15b4d3154b52e6ff022ef
yangy <yangy@DESKTOP-BC9KLPC>
2018-05-24 21:27:04 +0800

    small change.

--- src/main/java/randoop/generation/date/mutation/operation/StringAlterModify.java
+++ src/main/java/randoop/generation/date/mutation/operation/StringAlterModify.java
@@ -3,6 +3,7 @@ package randoop.generation.date.mutation.operation;
 import java.util.Map;
 
 import cern.colt.matrix.impl.DenseObjectMatrix2D;
+import randoop.generation.date.mutation.util.StatementInfoFetcher;
 import randoop.generation.date.sequence.TraceableSequence;
 import randoop.operation.TypedOperation;
 
@@ -35,7 +36,18 @@ public class StringAlterModify extends MutationOperation {
 	@Override
 	public DenseObjectMatrix2D toComputeTensor(Map<TypedOperation, Integer> operation_id_map,
 			Map<String, Integer> other_value_id_map) {
-		
-		return null;
+		DenseObjectMatrix2D result = new DenseObjectMatrix2D(2, 5);
+		result.set(0, 0, stmtIndex);
+		result.set(1, 0, 2);
+		int operation_index = operation_id_map.size() + other_value_id_map.get("StringInsertModify");
+		result.set(0, 1, operation_index);
+		result.set(1, 1, 1);
+		result.set(0, 2, StatementInfoFetcher.FetchVariableDeclareStatementIndex(sequence, stmtIndex, varIndex));
+		result.set(1, 2, 0);
+		result.set(0, 3, other_value_id_map.get("CharIndex" + charIndex));
+		result.set(1, 3, 1);
+		result.set(0, 4, other_value_id_map.get(deltaValue + ""));
+		result.set(1, 4, 1);
+		return result;
 	}
 }
--- src/main/java/randoop/generation/date/mutation/operation/StringInsertModify.java
+++ src/main/java/randoop/generation/date/mutation/operation/StringInsertModify.java
@@ -3,6 +3,7 @@ package randoop.generation.date.mutation.operation;
 import java.util.Map;
 
 import cern.colt.matrix.impl.DenseObjectMatrix2D;
+import randoop.generation.date.mutation.util.StatementInfoFetcher;
 import randoop.generation.date.sequence.TraceableSequence;
 import randoop.operation.TypedOperation;
 
@@ -11,7 +12,6 @@ public class StringInsertModify extends MutationOperation {
 	int stmtIndex = -1;
 	int varIndex = -1;
 	int charIndex = -1;
-	int value = ' ';
 
 	public StringInsertModify(TraceableSequence sequence, int stmtIndex, int varIndex, int charIndex) {
 		super(sequence);
@@ -33,7 +33,16 @@ public class StringInsertModify extends MutationOperation {
 	@Override
 	public DenseObjectMatrix2D toComputeTensor(Map<TypedOperation, Integer> operation_id_map,
 			Map<String, Integer> other_value_id_map) {
-		
-		return null;
+		DenseObjectMatrix2D result = new DenseObjectMatrix2D(2, 4);
+		result.set(0, 0, stmtIndex);
+		result.set(1, 0, 2);
+		int operation_index = operation_id_map.size() + other_value_id_map.get("StringInsertModify");
+		result.set(0, 1, operation_index);
+		result.set(1, 1, 1);
+		result.set(0, 2, StatementInfoFetcher.FetchVariableDeclareStatementIndex(sequence, stmtIndex, varIndex));
+		result.set(1, 2, 0);
+		result.set(0, 3, other_value_id_map.get("CharIndex" + charIndex));
+		result.set(1, 3, 1);
+		return result;
 	}
 }
