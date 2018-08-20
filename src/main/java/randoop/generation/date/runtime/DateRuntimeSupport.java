438e580c09a2ff121c55b64271193fb63b874993
yangy <yangy@101.5.212.1>
2018-08-20 18:50:08 +0800

    small change.

---
+++ src/main/java/randoop/generation/date/runtime/DateRuntimeSupport.java
@@ -0,0 +1,83 @@
+package randoop.generation.date.runtime;
+
+import java.util.Random;
+
+public class DateRuntimeSupport {
+
+	private static Random random = new Random();
+
+	public static Boolean CreateBoolean() {
+		return random.nextBoolean();
+	}
+
+	public static Boolean not(Boolean b) {
+		return !b;
+	}
+
+	public static Character CreateCharacter() {
+		return getRandomCharacter('\u0000','\uFFFF');
+	}
+
+	public static Character add(Character x, Object delta) {
+		return (char) (x + (int) delta);
+	}
+	
+	public static Byte CreateByte() {
+		byte[] bytes = new byte[1];
+		random.nextBytes(bytes);
+		return (Byte)bytes[0];
+	}
+
+	public static Byte add(Byte x, Object delta) {
+		return (byte) (x + (int) delta);
+	}
+	
+	public static Short CreateShort() {
+		return (Short)(short)random.nextInt();
+	}
+
+	public static Short add(Short x, Object delta) {
+		return (short) (x + (int) delta);
+	}
+	
+	public static Integer CreateInteger() {
+		return random.nextInt();
+	}
+
+	public static Integer add(Integer x, Object delta) {
+		return x + (Integer) delta;
+	}
+	
+	public static Long CreateLong() {
+		return random.nextLong();
+	}
+
+	public static Long add(Long x, Object delta) {
+		return x + (Long) delta;
+	}
+	
+	public static Float CreateFloat() {
+		return random.nextFloat();
+	}
+
+	public static Float add(Float x, Object delta) {
+		return x + (Float) delta;
+	}
+	
+	public static Double CreateDouble() {
+		return random.nextDouble();
+	}
+
+	public static Double add(Double x, Object delta) {
+		return x + (Double) delta;
+	}
+	
+	public static StringBuilder CreateStringBuilder() {
+		return new StringBuilder();
+	}
+
+	private static char getRandomCharacter(char ch1, char ch2) {
+		return (char) (ch1 + Math.random() * (ch2 - ch1 + 1));// 因为random<1.0，所以需要+1，才能取到ch2
+	}
+
+}
