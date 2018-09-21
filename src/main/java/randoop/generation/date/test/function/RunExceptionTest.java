84157b494d686b521540fbc5b96927f9e4a0f1d9
yangy <yangy@101.5.220.52>
2018-09-21 13:16:53 +0800

    small change.

---
+++ src/main/java/randoop/generation/date/test/function/RunExceptionTest.java
@@ -0,0 +1,13 @@
+package randoop.generation.date.test.function;
+
+public class RunExceptionTest {
+	
+	@SuppressWarnings({ "unused", "rawtypes" })
+	public static void main(String[] args) {
+		java.util.LinkedList linkedList0 = new java.util.LinkedList();
+		int int1 = linkedList0.size();
+		Object e2 = linkedList0.peekFirst();
+		System.out.println(e2);
+	}
+	
+}
