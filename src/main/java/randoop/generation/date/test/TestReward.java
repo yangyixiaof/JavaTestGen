2fa7c240b94e9ab57bb0d6e4f95d174d5d1cffb0
yangy <yangy@DESKTOP-BC9KLPC>
2018-09-27 17:58:43 +0800

    small change.

---
+++ src/main/java/randoop/generation/date/test/TestReward.java
@@ -0,0 +1,22 @@
+package randoop.generation.date.test;
+
+public class TestReward {
+	
+	public TestReward() {
+	}
+	
+	public void test1() {
+		
+	}
+	
+	public void test2() {
+		
+	}
+	
+	public static void main(String[] args) {
+		TestReward tr = new TestReward();
+		tr.test1();
+		tr.test2();
+	}
+	
+}
