ca2059722dea83f5e30b1e7959ac57be22ea04c9
yangy <yangy@DESKTOP-BC9KLPC>
2018-09-30 02:54:32 +0800

    small change.

--- src/main/java/randoop/generation/date/random/RandomSelect.java
+++ src/main/java/randoop/generation/date/random/RandomSelect.java
@@ -22,45 +22,58 @@ public class RandomSelect {
 
 	public static Random random = new Random();
 
-//	private static <T> TotalAlignedResult<T> GenerateTotalAlignedMap(Map<T, Double> wait_select) {
-//		List<Map.Entry<T, Double>> list = new LinkedList<>(wait_select.entrySet());
-//		Collections.sort(list, new Comparator<Map.Entry<T, Double>>() {
-//			@Override
-//			public int compare(Map.Entry<T, Double> o1, Map.Entry<T, Double> o2) {
-//				return -(o1.getValue()).compareTo(o2.getValue());
-//			}
-//		});
-//
-//		double max_r = 10.0;
-//		double min_r = 0.0;
-//		double gap_r = (max_r - min_r) / ((list.size() + 1) * 1.0);
-//		double r = max_r;
-//
-//		Map<T, Double> aligned_map = new HashMap<T, Double>();
-//
-//		double total_rewards = 0.0;
-//		double prev_reward = 0.0;
-//		Iterator<Map.Entry<T, Double>> kitr = list.iterator();
-//		while (kitr.hasNext()) {
-//			Map.Entry<T, Double> entry = kitr.next();
-//			T curr_t = entry.getKey();
-//			double curr_reward = entry.getValue();
-//			if (curr_reward != prev_reward) {
-//				r -= gap_r;
-//			}
-//			double r_r = Math.pow(r, 2.5);
-//			total_rewards += r_r;
-//			aligned_map.put(curr_t, r_r);
-//		}
-//		return new TotalAlignedResult<T>(total_rewards, aligned_map);
-//	}
+	// private static <T> TotalAlignedResult<T> GenerateTotalAlignedMap(Map<T,
+	// Double> wait_select) {
+	// List<Map.Entry<T, Double>> list = new LinkedList<>(wait_select.entrySet());
+	// Collections.sort(list, new Comparator<Map.Entry<T, Double>>() {
+	// @Override
+	// public int compare(Map.Entry<T, Double> o1, Map.Entry<T, Double> o2) {
+	// return -(o1.getValue()).compareTo(o2.getValue());
+	// }
+	// });
+	//
+	// double max_r = 10.0;
+	// double min_r = 0.0;
+	// double gap_r = (max_r - min_r) / ((list.size() + 1) * 1.0);
+	// double r = max_r;
+	//
+	// Map<T, Double> aligned_map = new HashMap<T, Double>();
+	//
+	// double total_rewards = 0.0;
+	// double prev_reward = 0.0;
+	// Iterator<Map.Entry<T, Double>> kitr = list.iterator();
+	// while (kitr.hasNext()) {
+	// Map.Entry<T, Double> entry = kitr.next();
+	// T curr_t = entry.getKey();
+	// double curr_reward = entry.getValue();
+	// if (curr_reward != prev_reward) {
+	// r -= gap_r;
+	// }
+	// double r_r = Math.pow(r, 2.5);
+	// total_rewards += r_r;
+	// aligned_map.put(curr_t, r_r);
+	// }
+	// return new TotalAlignedResult<T>(total_rewards, aligned_map);
+	// }
 
-//	private static <T> T RandomKeyFromMapByValue(Map<T, Double> wait_select) {
-//		// sort, big first
-//		return RandomKeyFromAlignedResult(GenerateTotalAlignedMap(wait_select));
-//	}
-	
+	// private static <T> T RandomKeyFromMapByValue(Map<T, Double> wait_select) {
+	// // sort, big first
+	// return RandomKeyFromAlignedResult(GenerateTotalAlignedMap(wait_select));
+	// }
+
 	private static <T> T RandomKeyFromMapByValue(Map<T, Double> wait_select) {
+		{
+			// print rewards
+			System.out.println("===== reward print begin =====");
+			Set<T> w_ks = wait_select.keySet();
+			Iterator<T> w_k_itr = w_ks.iterator();
+			while (w_k_itr.hasNext()) {
+				T t = w_k_itr.next();
+				Double r = wait_select.get(t);
+				System.out.println("t:" + t + "#reward:" + r);
+			}
+			System.out.println("===== reward print end =====");
+		}
 		// sort, big first
 		Map<T, Double> non_zero_wait_select = new HashMap<T, Double>();
 		Map<T, Double> zero_wait_select = new HashMap<T, Double>();
@@ -80,9 +93,9 @@ public class RandomSelect {
 		if (real_rate < 0.68) {
 			real_rate = 0.68;
 		}
-//		if (real_rate > 0.8) {
-//			real_rate = 0.8;
-//		}
+		// if (real_rate > 0.8) {
+		// real_rate = 0.8;
+		// }
 		boolean sample_from_non_zero = false;
 		if (non_zero_wait_select.size() > 0) {
 			if (Math.random() <= real_rate) {
@@ -112,24 +125,25 @@ public class RandomSelect {
 		}
 	}
 
-//	public static <T> T RandomKeyFromAlignedResult(TotalAlignedResult<T> aligned) {
-//		double total_rewards = aligned.total_rewards;
-//		Map<T, Double> aligned_map = aligned.aligned_map;
-//		double select_double = random.nextDouble() * total_rewards;
-//		Set<T> aks = aligned_map.keySet();
-//		double accumulated_rewards = 0.0;
-//		Iterator<T> ak_itr = aks.iterator();
-//		T t = null;
-//		while (ak_itr.hasNext()) {
-//			t = ak_itr.next();
-//			Double reward = aligned_map.get(t);
-//			accumulated_rewards += reward;
-//			if (select_double < accumulated_rewards) {
-//				break;
-//			}
-//		}
-//		return t;
-//	}
+	// public static <T> T RandomKeyFromAlignedResult(TotalAlignedResult<T> aligned)
+	// {
+	// double total_rewards = aligned.total_rewards;
+	// Map<T, Double> aligned_map = aligned.aligned_map;
+	// double select_double = random.nextDouble() * total_rewards;
+	// Set<T> aks = aligned_map.keySet();
+	// double accumulated_rewards = 0.0;
+	// Iterator<T> ak_itr = aks.iterator();
+	// T t = null;
+	// while (ak_itr.hasNext()) {
+	// t = ak_itr.next();
+	// Double reward = aligned_map.get(t);
+	// accumulated_rewards += reward;
+	// if (select_double < accumulated_rewards) {
+	// break;
+	// }
+	// }
+	// return t;
+	// }
 
 	public static Object RandomElementFromSetByRewardableElements(Collection<? extends Rewardable> wait_select,
 			ArrayList<String> interested_branch, SelectFileter<?> filter) {
@@ -161,58 +175,62 @@ public class RandomSelect {
 		return RandomKeyFromMapByValue(n_r_final_wait_select);
 	}
 
-//	public static <T> T GetBestKeyFromMapByRewardableValue(Map<T, ? extends Rewardable> wait_select,
-//			ArrayList<String> interested_branch) {
-//		Map<T, Reward> final_wait_select = new HashMap<T, Reward>();
-//		Set<T> w_keys = wait_select.keySet();
-//		Iterator<T> kitr = w_keys.iterator();
-//		while (kitr.hasNext()) {
-//			T k = kitr.next();
-//			Rewardable t = wait_select.get(k);
-//			Reward reward = t.GetReward(interested_branch);
-//			final_wait_select.put(k, reward);
-//		}
-//		if (final_wait_select.size() == 0) {
-//			return null;
-//		}
-//		NormalizeUtil.NormalizeRewards(final_wait_select);
-//		Map<T, Double> r_final_wait_select = RewardUtil.RewardValueToDoubleValueInMap(final_wait_select);
-//		return MaxValueKey(r_final_wait_select);
-//	}
-//
-//	public static Object GetBestElementFromSetByRewardableElement(Collection<? extends Rewardable> wait_select,
-//			ArrayList<String> interested_branch, SelectFileter<Object> filter) {
-//		Map<Object, Reward> final_wait_select = new HashMap<Object, Reward>();
-//		Iterator<? extends Rewardable> kitr = wait_select.iterator();
-//		while (kitr.hasNext()) {
-//			Rewardable t = kitr.next();
-//			if (filter == null || filter.Retain((Object) t)) {
-//				Reward reward = t.GetReward(interested_branch);
-//				final_wait_select.put(t, reward);
-//				// if (t instanceof PseudoSequenceContainer) {
-//				// System.out.println("===== reward:" + reward + " =====");
-//				// System.out.println(t);
-//				// }
-//			}
-//		}
-//		if (final_wait_select.size() == 0) {
-//			return null;
-//		}
-//		NormalizeUtil.NormalizeRewards(final_wait_select);
-//		Map<Object, Double> r_final_wait_select = RewardUtil.RewardValueToDoubleValueInMap(final_wait_select);
-//		return MaxValueKey(r_final_wait_select);
-//	}
+	// public static <T> T GetBestKeyFromMapByRewardableValue(Map<T, ? extends
+	// Rewardable> wait_select,
+	// ArrayList<String> interested_branch) {
+	// Map<T, Reward> final_wait_select = new HashMap<T, Reward>();
+	// Set<T> w_keys = wait_select.keySet();
+	// Iterator<T> kitr = w_keys.iterator();
+	// while (kitr.hasNext()) {
+	// T k = kitr.next();
+	// Rewardable t = wait_select.get(k);
+	// Reward reward = t.GetReward(interested_branch);
+	// final_wait_select.put(k, reward);
+	// }
+	// if (final_wait_select.size() == 0) {
+	// return null;
+	// }
+	// NormalizeUtil.NormalizeRewards(final_wait_select);
+	// Map<T, Double> r_final_wait_select =
+	// RewardUtil.RewardValueToDoubleValueInMap(final_wait_select);
+	// return MaxValueKey(r_final_wait_select);
+	// }
+	//
+	// public static Object GetBestElementFromSetByRewardableElement(Collection<?
+	// extends Rewardable> wait_select,
+	// ArrayList<String> interested_branch, SelectFileter<Object> filter) {
+	// Map<Object, Reward> final_wait_select = new HashMap<Object, Reward>();
+	// Iterator<? extends Rewardable> kitr = wait_select.iterator();
+	// while (kitr.hasNext()) {
+	// Rewardable t = kitr.next();
+	// if (filter == null || filter.Retain((Object) t)) {
+	// Reward reward = t.GetReward(interested_branch);
+	// final_wait_select.put(t, reward);
+	// // if (t instanceof PseudoSequenceContainer) {
+	// // System.out.println("===== reward:" + reward + " =====");
+	// // System.out.println(t);
+	// // }
+	// }
+	// }
+	// if (final_wait_select.size() == 0) {
+	// return null;
+	// }
+	// NormalizeUtil.NormalizeRewards(final_wait_select);
+	// Map<Object, Double> r_final_wait_select =
+	// RewardUtil.RewardValueToDoubleValueInMap(final_wait_select);
+	// return MaxValueKey(r_final_wait_select);
+	// }
 
-//	public static <T> T MaxValueKey(Map<T, Double> wait_select) {
-//		List<Map.Entry<T, Double>> list = new LinkedList<>(wait_select.entrySet());
-//		Collections.sort(list, new Comparator<Map.Entry<T, Double>>() {
-//			@Override
-//			public int compare(Map.Entry<T, Double> o1, Map.Entry<T, Double> o2) {
-//				return -(o1.getValue()).compareTo(o2.getValue());
-//			}
-//		});
-//		return list.get(0).getKey();
-//	}
+	// public static <T> T MaxValueKey(Map<T, Double> wait_select) {
+	// List<Map.Entry<T, Double>> list = new LinkedList<>(wait_select.entrySet());
+	// Collections.sort(list, new Comparator<Map.Entry<T, Double>>() {
+	// @Override
+	// public int compare(Map.Entry<T, Double> o1, Map.Entry<T, Double> o2) {
+	// return -(o1.getValue()).compareTo(o2.getValue());
+	// }
+	// });
+	// return list.get(0).getKey();
+	// }
 
 	public static <T> T RandomKeyFromMapByRewardableValue(Map<T, ? extends Rewardable> wait_select,
 			ArrayList<String> interested_branch, SelectFileter<T> filter) {
@@ -231,50 +249,54 @@ public class RandomSelect {
 		if (final_wait_select.size() == 0) {
 			return null;
 		}
-//		NormalizeUtil.NormalizeRewards(final_wait_select);
-//		Map<T, Double> r_final_wait_select = RewardUtil.RewardValueToDoubleValueInMap(final_wait_select);
+		// NormalizeUtil.NormalizeRewards(final_wait_select);
+		// Map<T, Double> r_final_wait_select =
+		// RewardUtil.RewardValueToDoubleValueInMap(final_wait_select);
 		Map<T, Reward> normal_final_wait_select = NormalizeUtil.ProbabilizeRewards(final_wait_select);
 		Map<T, Double> r_final_wait_select = RewardUtil.RewardValueToDoubleValueInMap(normal_final_wait_select);
 		Map<T, Double> n_r_final_wait_select = NormalizeUtil.ProbabilizeDoubleValues(r_final_wait_select);
 		return RandomKeyFromMapByValue(n_r_final_wait_select);
 	}
 
-//	public static <T> T RandomKeyFromMapByRewardableValueWithPenalizableValue(Map<T, ? extends Rewardable> wait_select,
-//			Map<T, ? extends Penalizable> punish, ArrayList<String> interested_branch, SelectFileter<T> filter,
-//			TypedOperation to) {
-//		Map<T, Double> final_wait_select = new HashMap<T, Double>();
-//		Set<T> keys = wait_select.keySet();
-//		Iterator<T> kitr = keys.iterator();
-//		System.out.println("=== start ===");
-//		while (kitr.hasNext()) {
-//			T t = kitr.next();
-//			if (filter == null || filter.Retain(t)) {
-//				System.out.println("Retained:" + t);
-//				double reward = 0.0;
-//				Rewardable to_branch_influence = wait_select.get(t);
-//				if (to_branch_influence != null) {
-//					reward += to_branch_influence.GetReward(interested_branch);
-//				}
-//				double punish_v = 0.0;
-//				Penalizable punish_val = punish.get(t);
-//				if (punish_val != null) {
-//					punish_v += punish_val.GetPunishment(to);
-//				}
-//				if (punish_v >= 0) {
-//					final_wait_select.put(t, reward);
-//				}
-//			}
-//		}
-//		if (final_wait_select.size() == 0) {
-//			return null;
-//		}
-//		return RandomKeyFromMapByValue(final_wait_select);
-//	}
+	// public static <T> T
+	// RandomKeyFromMapByRewardableValueWithPenalizableValue(Map<T, ? extends
+	// Rewardable> wait_select,
+	// Map<T, ? extends Penalizable> punish, ArrayList<String> interested_branch,
+	// SelectFileter<T> filter,
+	// TypedOperation to) {
+	// Map<T, Double> final_wait_select = new HashMap<T, Double>();
+	// Set<T> keys = wait_select.keySet();
+	// Iterator<T> kitr = keys.iterator();
+	// System.out.println("=== start ===");
+	// while (kitr.hasNext()) {
+	// T t = kitr.next();
+	// if (filter == null || filter.Retain(t)) {
+	// System.out.println("Retained:" + t);
+	// double reward = 0.0;
+	// Rewardable to_branch_influence = wait_select.get(t);
+	// if (to_branch_influence != null) {
+	// reward += to_branch_influence.GetReward(interested_branch);
+	// }
+	// double punish_v = 0.0;
+	// Penalizable punish_val = punish.get(t);
+	// if (punish_val != null) {
+	// punish_v += punish_val.GetPunishment(to);
+	// }
+	// if (punish_v >= 0) {
+	// final_wait_select.put(t, reward);
+	// }
+	// }
+	// }
+	// if (final_wait_select.size() == 0) {
+	// return null;
+	// }
+	// return RandomKeyFromMapByValue(final_wait_select);
+	// }
 
 	public static PseudoVariable RandomPseudoVariableListAccordingToLength(ArrayList<PseudoVariable> pvs) {
 		Map<PseudoVariable, Double> wait_select = new HashMap<PseudoVariable, Double>();
 		for (PseudoVariable pv : pvs) {
-			double reward = pv.sequence.SizeOfUsers() * 0.5 + pv.sequence.Size()*0.25 + pv.index * 0.5 + 0.1;
+			double reward = pv.sequence.SizeOfUsers() * 0.5 + pv.sequence.Size() * 0.25 + pv.index * 0.5 + 0.1;
 			wait_select.put(pv, 1 / reward);
 		}
 		return RandomKeyFromMapByValue(wait_select);
@@ -282,14 +304,14 @@ public class RandomSelect {
 
 }
 
-//class TotalAlignedResult<T> {
+// class TotalAlignedResult<T> {
 //
-//	double total_rewards;
-//	Map<T, Double> aligned_map;
+// double total_rewards;
+// Map<T, Double> aligned_map;
 //
-//	public TotalAlignedResult(double total, Map<T, Double> aligned_map) {
-//		this.total_rewards = total;
-//		this.aligned_map = aligned_map;
-//	}
+// public TotalAlignedResult(double total, Map<T, Double> aligned_map) {
+// this.total_rewards = total;
+// this.aligned_map = aligned_map;
+// }
 //
-//}
+// }
