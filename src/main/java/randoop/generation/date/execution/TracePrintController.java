ada1b6ec904fe3ea30482bd2e3f5ba3d1191a4d9
yangy <yangy@DESKTOP-BC9KLPC>
2018-10-04 13:00:39 +0800

    small change.

--- src/main/java/randoop/generation/date/execution/TracePrintController.java
+++ src/main/java/randoop/generation/date/execution/TracePrintController.java
@@ -1,8 +1,6 @@
 package randoop.generation.date.execution;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 public class TracePrintController {
 	
@@ -14,11 +12,11 @@ public class TracePrintController {
 			/**
 			 * print operation start
 			 */
-			Method m = c.getMethod("PrintOperationStartUp");
-			m.invoke(null);
+//			Method m = c.getMethod("PrintOperationStartUp");
+//			m.invoke(null);
 //			Boolean f_v = (Boolean)f.get(null);
 //			System.out.println("open setted f_v:" + f_v);
-		} catch (ClassNotFoundException | NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+		} catch (ClassNotFoundException | NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
 			e.printStackTrace();
 		}
 	}
