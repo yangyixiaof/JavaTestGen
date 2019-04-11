43ffd4780822eb942673a99bad706cd1e53c0232
yixiao yang <yangyixiaofirst@163.com>
2019-04-11 15:31:07 +0800

    small change.

--- src/main/java/randoop/generation/date/test/resource/TestString.java
+++ src/main/java/randoop/generation/date/test/resource/TestString.java
@@ -3,7 +3,11 @@ package randoop.generation.date.test.resource;
 public class TestString {
 	
 	public static void TestDriver(String str) {
-		if (Integer.parseInt(str) == 4) {
+//		if (Integer.parseInt(str) == 4) {
+//			System.out.println("4 encountered!");
+//		}
+//		System.out.println("not encountered!");
+		if (str.charAt(0) == '4') {
 			System.out.println("4 encountered!");
 		}
 		System.out.println("not encountered!");
